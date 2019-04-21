1a1b8d1d5224ac57b939d6351055d25c33cc3acf
asafari72 <asafari72@gmail.com>
2019-04-21 10:54:24 -0700

    added sass support

---
+++ src/text-box/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./index.scss";
+export class TextBox extends Component {
+  render() {
+    return (
+      <div>
+        <input />
+      </div>
+    );
+  }
+}
+
+export default TextBox;
