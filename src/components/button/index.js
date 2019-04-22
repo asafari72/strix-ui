ee7dda5a63f0894c496bfb3732d0cead20e49a51
mohamad hasanzadeh <m.hasanzadeh@istd.com>
2019-04-22 14:22:57 +0430

    button added.

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-
+import "./index.scss";
 export class Button extends Component {
   render() {
+    const { left, right } = this.props;
     return (
-      <div>
-        <button>Hi</button>
+      <div className="button-container">
+        <button>{this.props.name}</button>
       </div>
     );
   }
