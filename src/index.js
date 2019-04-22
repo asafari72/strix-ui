d8e4f96aec7859e7059a9686b43a20a09eebcc8f
a.safari <a.safari@SAFARIA>
2019-04-22 11:10:57 +0430

    added iransans font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import './styles/index.scss';
+import "./styles/index.scss";
+import "iran-sans-font-num/css/style.scss";
 
 import { TextBox } from "./components/text-box";
-import { IconBadge } from './components/icon-badge'
+import { IconBadge } from "./components/icon-badge";
 
-
-
-
-
-export { TextBox,IconBadge };
+export { TextBox, IconBadge };
