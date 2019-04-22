8e1a0e55861f44b40097563e71464e387aa945a1
a.safari <a.safari@SAFARIA>
2019-04-22 14:24:08 +0430

    input css

--- src/components/text-box/index.js
+++ src/components/text-box/index.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
 import "./index.scss";
 export class TextBox extends Component {
+  state = {
+    isFoucs: false
+  };
   render() {
+    const { isFoucs } = this.state;
     const { left, right } = this.props;
     return (
-      <div className="input-container">
+      <div
+        className={isFoucs ? "input-container is-focused" : "input-container"}
+      >
         {right ? <div>{right}</div> : ""}
         <div>
-          <input onFocus={this.onFocus} {...this.props} />
+          <input onFocus={this.onFocus} onBlur={this.onBlur} {...this.props} />
         </div>
         {left ? <div>{left}</div> : ""}
       </div>
     );
   }
   onFocus = () => {
-    console.log("object");
+    this.setState({ isFoucs: true });
+  };
+  onBlur = () => {
+    this.setState({ isFoucs: false });
   };
 }
 
