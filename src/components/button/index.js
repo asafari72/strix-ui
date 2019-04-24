edb8aedeb2292b200fe00b691cf91f05b0958b4a
a.safari <a.safari@SAFARIA>
2019-04-24 12:41:31 +0430

    change button class to memo

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,69 +1,23 @@
 import React, { Component } from "react";
 import "./index.scss";
-export class Button extends Component {
-  state = {
-    type: "primary",
-    size: "normal"
-  };
-  componentDidMount() {
-    switch (this.props.type) {
-      case "success":
-        this.setState({ type: "success" });
-        break;
-      case "warning":
-        this.setState({ type: "warning" });
-        break;
-      case "danger":
-        this.setState({ type: "danger" });
-        break;
-      case "primary-outline":
-        this.setState({ type: "primary-outline" });
-        break;
-      case "success-outline":
-        this.setState({ type: "success-outline" });
-        break;
-      case "warning-outline":
-        this.setState({ type: "warning-outline" });
-        break;
-      case "danger-outline":
-        this.setState({ type: "danger-outline" });
-        break;
-      default:
-        this.setState({ type: "primary" });
-        break;
-    }
-    switch (this.props.size) {
-      case "small":
-        this.setState({ size: "small" });
-        break;
-      case "big":
-        this.setState({ size: "big" });
-        break;
-      default:
-        this.setState({ size: "normal" });
-        break;
-    }
-  }
-  render() {
-    console.log(this.state);
-    const { icon, name, loading } = this.props;
-    const { type, size } = this.state;
-    return (
-      <div className={"button-container " + "btn-" + type + " btn-" + size}>
-        {icon ? (
-          <div className="button-right">
-            <i className={"fas fa-" + icon} />
-          </div>
-        ) : (
-          ""
-        )}
-        <button {...this.props} disabled={loading}>
-          {" "}
-          {loading ? <i className="fas fa-circle-notch fa-spin fa-lg	" /> : name}
-        </button>
-      </div>
-    );
-  }
-}
 
-export default Button;
+const Button = React.memo(function Button(
+  { name, icon, onClick, loading = false, type = "primary", size = "normal" },
+  props
+) {
+  return (
+    <div className={"button-container " + "btn-" + type + " btn-" + size}>
+      {icon ? (
+        <div className="button-right">
+          <i className={"fas fa-" + icon} />
+        </div>
+      ) : (
+        ""
+      )}
+      <button disabled={loading} onClick={onClick}>
+        {loading ? <i className="fas fa-circle-notch fa-spin fa-lg" /> : name}
+      </button>
+    </div>
+  );
+});
+export { Button };
--- src/components/notify/index.js
+++ src/components/notify/index.js
@@ -1,16 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import "./index.scss";
-const Notify = (
+
+const Notify = React.memo(function Notify(
   { title, message, type = "primary", closeable = false, show = false },
   props
-) => {
+) {
   return show ? (
-    <div className={"notify-container bk-" + type}>
-      {title ? <header>{title}</header> : ""}
-      {message}
+    <div
+      id="notify-container"
+      className={title ? "notify-container" : "notify-container rounded"}
+    >
+      {title ? (
+        <header className={"bk-" + type}>
+          {title}
+          {closeable ? (
+            <i
+              className="fas fa-times"
+              onClick={() => {
+                show = false;
+                console.log("show");
+              }}
+            />
+          ) : (
+            ""
+          )}
+        </header>
+      ) : (
+        ""
+      )}
+      <span className={!title ? "bk-" + type : ""}>{message}</span>
     </div>
   ) : (
     ""
   );
-};
+});
 export { Notify };
